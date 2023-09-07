530eaeb4f21e6c66e42ae7f5c955c89a102af1cb
Devoodie <131494134+Devoodie@users.noreply.github.com>
2023-09-07 16:14:54 -0400

    TODO>> Need to finish deckMaker function. created the suite initialization!

--- cards.h
+++ cards.h
@@ -2,25 +2,30 @@
 // Created by devan on 9/6/2023.
 //
 #include <string>
+#include <utility>
 #include <vector>
 #ifndef POKERHAND_CARDS_H
 #define POKERHAND_CARDS_H
-std::string suites[4] = {"Spades","Hearts", "Diamonds", "Clubs"};
+using namespace std;
+string suites[4] = {"Spades","Hearts", "Diamonds", "Clubs"};
 class cards {
-    std::string name;
+    string name;
     unsigned number;
-    std::string suite;
+    string suite;
     public:
-        cards(std::string n, std::string s, unsigned val):
-            number(val), suite(s), name(n) {};
-};
+        cards(string n, string s, unsigned val):
+            number(val), suite(std::move(s)), name(std::move(n)) {};
+        vector<cards> deckMaker( string suites[]){
+            vector<cards> deck;
+            for(int i = 2; i <= 15; ++i)
+                cards spade = cards(to_string(i)+"of"+suites[0], suites[0],i),
+                heart = cards(to_string(i)+"of"+suites[1], suites[1],i),
+                diamond = cards(to_string(i)+"of"+suites[2], suites[2],i),
+                club = cards(to_string(i)+"of"+suites[3], suites[3],i); //append tro deck
+            return deck;
+    };
+    vector<cards> deck;
 
-std::vector<cards> deckMaker(){
-    std::vector<cards> deck;
-    for(int i = 0; i <= 12; ++i)
-        deck.push_back(cards());
-
-    return deck;
 };
 
 #endif //POKERHAND_CARDS_H
