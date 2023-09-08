35bde1b6ba8228f4a08eb19a51b959865abc7920
Devoodie <131494134+Devoodie@users.noreply.github.com>
2023-09-07 23:31:23 -0400

    finished the deckmaker function

--- cards.h
+++ cards.h
@@ -7,7 +7,7 @@
 #ifndef POKERHAND_CARDS_H
 #define POKERHAND_CARDS_H
 using namespace std;
-string suites[4] = {"Spades","Hearts", "Diamonds", "Clubs"};
+
 class cards {
     string name;
     unsigned number;
@@ -15,17 +15,23 @@ class cards {
     public:
         cards(string n, string s, unsigned val):
             number(val), suite(std::move(s)), name(std::move(n)) {};
-        vector<cards> deckMaker( string suites[]){
-            vector<cards> deck;
-            for(int i = 2; i <= 15; ++i)
-                cards spade = cards(to_string(i)+"of"+suites[0], suites[0],i),
-                heart = cards(to_string(i)+"of"+suites[1], suites[1],i),
-                diamond = cards(to_string(i)+"of"+suites[2], suites[2],i),
-                club = cards(to_string(i)+"of"+suites[3], suites[3],i); //append tro deck
-            return deck;
     };
-    vector<cards> deck;
 
+vector<cards> deckMaker() {
+    vector<cards> deck;
+    string suites[4] = {"Spades","Hearts", "Diamonds", "Clubs"};
+    for (int i = 2; i <= 15; ++i) {
+        cards spade = cards(to_string(i) + "of" + suites[0], suites[0], i),
+                heart = cards(to_string(i) + "of" + suites[1], suites[1], i),
+                diamond = cards(to_string(i) + "of" + suites[2], suites[2], i),
+                club = cards(to_string(i) + "of" + suites[3], suites[3], i);
+        deck.push_back(spade);
+        deck.push_back(heart);
+        deck.push_back(diamond);
+        deck.push_back(club);
+    };
+    return deck;
 };
+
 
 #endif //POKERHAND_CARDS_H
